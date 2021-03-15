84f3af21100b5805e9e6f614341bee3cbd57e421
sahelea1 <jonathan.schopper@yandex.com>
2021-03-15 11:51:07 +0100

    added Get + put request to api

--- Server/api/serve.py
+++ Server/api/serve.py
@@ -1,38 +1,61 @@
 import flask
 from flask import request, jsonify
+import sqlite3
+import os
+import pandas as pd
+from pandas import DataFrame
+from datetime import date
+from datetime import datetime
+import glob
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>404</h1>
+<p>not found</p>'''
 
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@app.route('/api/v1/getnmsgs/', methods=['GET'])
+def get_new_msgs():
+    #args=%APIKEY%...%id/msg%
+    argsn = int(request.args['args'])
+    argss = argsn.split("...")
+    if str(argss[1]) in open("api.keys"):
+        newestMsgId = argss[1]
+        curm = sqlite3.connect('./cmbs.db')
+        cur = curm.cursor()
+        cur.execute("SELECT * FROM MESSAGES WHERE gen_id>=" + str(newestMsgId))
+        rows = cur.fetchall()
+        return str(rows)
+    else:
+        return "API-Key invalid"
+
+
+@app.route('/api/v1/putmsg/', methods=['GET'])
+def get_new_msgs():
+    #args=%APIKEY%...%msg%
+    argsn = int(request.args['args'])
+    argss = argsn.split("...")
+    if str(argss[1]) in open("api.keys"):
+        msg = argss[1]
+        curm = sqlite3.connect('./cmbs.db')
+        cur = curm.cursor()
+        cur.execute("SELECT * FROM MESSAGES ORDER BY gen_id DESC")
+        rows = cur.fetchall()
+        fetched = False
+        for row in rows:
+            if not fetched:
+                last_row = row
+                
+                fetched = True
+    else:
+        return "API-Key invalid"
+
+
+
 
 app.run()
